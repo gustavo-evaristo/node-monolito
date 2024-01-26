bc4e623aee832c8b549c577a352645b332206ce6
Gustavo Evaristo <gug.henri1@gmail.com>
2024-01-26 09:44:20 -0300

    feat: factory and facade

---
+++ src/modules/invoice/facade/invoice.facade.interface.ts
@@ -0,0 +1,13 @@
+import {
+  FindInvoiceFacadeInputDTO,
+  FindInvoiceFacadeOutputDTO,
+  GenerateInvoiceFacadeInputDTO,
+  GenerateInvoiceFacadeOutputDTO,
+} from "./invoice.facade.dto";
+
+export default interface InvoiceFacadeInterface {
+  find(input: FindInvoiceFacadeInputDTO): Promise<FindInvoiceFacadeOutputDTO>;
+  generate(
+    input: GenerateInvoiceFacadeInputDTO
+  ): Promise<GenerateInvoiceFacadeOutputDTO>;
+}
